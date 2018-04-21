c5fed58ea51f3c0099501fb80d37a04e2f5b564a
Victor Sergeyev <victor17011998@yandex.ru>
2018-04-21 20:47:13 +0400

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email', 'password',
+        'first_name', 'last_name'
     ];
 
     /**
